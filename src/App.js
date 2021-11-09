7bd17f1193cb533154b39d180036e8bd9537cc84
Florian Weißmeier <flo@flowei.tech>
2021-11-09 22:26:07 +0100

    Modified App Component for Stateful Navigation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
-import FwButton from './components/FwButton';
+import FwNavbar from './components/FwNavbar/FwNavbar';
+import FwNavbarLink from './components/FwNavbar/FwNavbarLink/FwNavbarLink';
+import FwLandingPage from './components/FwLandingPage/FwLandingPage';import React from 'react';
+import FwProjectsPage from './components/FwProjectsPage/FwProjectsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <FwButton text="TEsto" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0,
+      activeSite: "landing"
+    }
+    this.handleTabClick = this.handleTabClick.bind(this)
+    this.ActiveSite = this.ActiveSite.bind(this)
+  }
+
+  handleTabClick (id, target) {
+    
+    this.setState({
+      activeTab: id,
+      activeSite: target
+    });
+    console.log("Changed Tab!")
+    console.log(id)
+    console.log(this.state.activeTab)
+  }
+
+  ActiveSite() {
+    switch (this.state.activeSite) {
+      case "landing":
+        return <FwLandingPage />
+      
+      case "projects":
+        return <FwProjectsPage />
+      
+      case "contact":
+        
+        break;
+    
+      default:
+        return <FwLandingPage />
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FwNavbar logo={logo}>
+          <FwNavbarLink activeTabId={this.state.activeTab} id={0} onClick={this.handleTabClick} target="landing">Welcome</FwNavbarLink>
+          <FwNavbarLink activeTabId={this.state.activeTab} id={1} onClick={this.handleTabClick} target="projects">Projects</FwNavbarLink>
+          <FwNavbarLink activeTabId={this.state.activeTab} id={2} onClick={this.handleTabClick} target="landing">Contact</FwNavbarLink>
+        </FwNavbar>
+        <this.ActiveSite />
+      </div>
+    );
+  }
 }
 
 export default App;
