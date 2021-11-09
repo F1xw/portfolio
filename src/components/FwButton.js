76a2e759a3bff0a02698aa7dc24656bd51085b27
Florian Weißmeier <flo@flowei.tech>
2021-11-09 15:25:30 +0100

    Added FwButton Component

---
+++ src/components/FwButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./FwButton.css";
+
+class FwButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.processClick = this.processClick.bind(this)
+  }
+
+  processClick() {
+    if (this.props.href !== undefined) {
+      window.open(this.props.href, '_blank')
+    }else if (this.props.onClick !== undefined) {
+      this.props.onClick()
+    }else{
+      return false;
+    }
+  }
+
+
+  render() {
+    return <button onClick={this.processClick} className="FwButton">{this.props.text}</button>;
+  }
+}
+
+export default FwButton;
