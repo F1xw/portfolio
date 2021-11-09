97e0e24c0bfe966c4fbd99fbb8aeda10f865e4c4
Florian Weißmeier <flo@flowei.tech>
2021-11-09 22:25:39 +0100

    Added Project Page Component

---
+++ src/components/FwProjectsPage/FwProjectsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./FwProjectsPage.css";
+
+class FwProjectsPage extends React.Component {
+
+    render() {
+        return <div className="content2">
+        </div>;
+    }
+
+}
+
+export default FwProjectsPage;
