43ae418883f1d5df1bcace962c5ac90e8611e639
Florian Weißmeier <flo@flowei.tech>
2021-11-09 22:25:11 +0100

    Added NavBarLink Component

---
+++ src/components/FwNavbar/FwNavbarLink/FwNavbarLink.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./FwNavbarLink.css";
+
+class FwNavbarLink extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: this.props.activeTabId };
+        this.handleClick = this.handleClick.bind(this)
+      }
+  
+  handleClick() {
+    this.props.onClick(this.props.id, this.props.target)
+  }
+
+  componentDidUpdate(oldProps) {
+    if (oldProps.activeTabId !== this.props.activeTabId) {
+      this.setState({
+        activeTab: this.props.activeTabId
+      });
+    }
+  } 
+    
+
+  render() {
+    let className = this.state.activeTab === this.props.id ? "FwNavbarLink active" : "FwNavbarLink"
+    return <li className={className} onClick={this.handleClick}>
+        <p className="FwNavbarLink__anchor">{this.props.children}</p>
+    </li>;
+  }
+}
+
+export default FwNavbarLink;
