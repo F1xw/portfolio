91e7efe946a0ac4e9a8fa06827df855d9370b8d7
Florian Weißmeier <flo@flowei.tech>
2022-05-13 15:50:09 +0200

    Removed humcast from project list

--- src/components/FwProjectsPage/FwProjectsPage.js
+++ src/components/FwProjectsPage/FwProjectsPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FaChevronRight } from 'react-icons/fa';
 import samcamProject from '../../assets/samcamProject.jpg';
-import humcastProject from '../../assets/humcastProject.jpg';
 import shareliProject from '../../assets/shareliProject.jpg';
 import p2pProject from '../../assets/p2pProject.jpg';
 import "./FwProjectsPage.scss";
@@ -16,7 +15,6 @@ class FwProjectsPage extends React.Component {
             activeIndex: null
         }
     }
-
 
     handleClick(e) {
         if (this.state.activeIndex === e.target.dataset.index) {
@@ -36,31 +34,6 @@ class FwProjectsPage extends React.Component {
                 <div className={parseInt(this.state.activeIndex) === 0 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={0}>
                     <FaChevronRight />
                     <div className="FwProjectsPage__projectItemText">
-                        <h1>Humcast</h1>
-                        <hr />
-                        <div className="FwProjectsPage__projectItemImage">
-                            <img src={humcastProject} alt="" />
-                        </div>
-                        <p>
-                        Humcast is a podcast made by students at my school (Humboldt-Gymnasium Solingen).
-                        I have made it my ambition to design (and manage) not only the logos and cover arts or the Instagram page but the website as well.
-                        While this portfolio was made using ReactJS I stuck with basic HTML, CSS and JS for the Humcast website
-                        to allow future generations of students to change their site easily. I used a CSS Framework to assist me in alignment and
-                        responsiveness and I used JQuery to ease my JavaScript development. I also coded a tiny CMS for Humcast,
-                        so that they can change text and images without touching any code.
-                            <br />
-                            <br />
-                            <p class="FwProjectsPage__projectItemLinks">
-                                <a href="https://humcast.de" target="_blank" rel="noreferrer">Humcast Website</a>
-                                &nbsp;•&nbsp; 
-                                <a href="https://open.spotify.com/show/567asgppRfjg9ApKZ77IHH" target="_blank" rel="noreferrer">Humcast Spotify</a>
-                            </p>
-                        </p>
-                    </div>
-                </div>
-                <div className={parseInt(this.state.activeIndex) === 1 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={1}>
-                    <FaChevronRight />
-                    <div className="FwProjectsPage__projectItemText">
                         <h1>SamCam</h1>
                         <hr />
                         <div className="FwProjectsPage__projectItemImage">
@@ -75,13 +48,13 @@ class FwProjectsPage extends React.Component {
                             enjoyed the process of coding, designing and building it.
                             <br />
                             <br />
-                            <p class="FwProjectsPage__projectItemLinks">
+                            <span className="FwProjectsPage__projectItemLinks">
                                 <a href="https://github.com/F1xw/samcam" target="_blank" rel="noreferrer">GitHub Repo</a>
-                            </p>
+                            </span>
                         </p>
                     </div>
                 </div>
-                <div className={parseInt(this.state.activeIndex) === 2 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={2}>
+                <div className={parseInt(this.state.activeIndex) === 1 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={1}>
                     <FaChevronRight />
                     <div className="FwProjectsPage__projectItemText">
                         <h1>Shareli.me</h1>
@@ -97,13 +70,13 @@ class FwProjectsPage extends React.Component {
                             ran out and I had security concerns about possible reverse shells or spamming.
                             <br />
                             <br />
-                            <p class="FwProjectsPage__projectItemLinks">
+                            <span className="FwProjectsPage__projectItemLinks">
                                 <a href="https://github.com/F1xw/shareli.me" target="_blank" rel="noreferrer">GitHub Repo</a>
-                            </p>
+                            </span>
                         </p>                   
                     </div>
                 </div>
-                <div className={parseInt(this.state.activeIndex) === 3 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={3}>
+                <div className={parseInt(this.state.activeIndex) === 2 ? "FwProjectsPage__projectItem active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={2}>
                     <FaChevronRight />
                     <div className="FwProjectsPage__projectItemText">
                         <h1>Peer-to-Peer Chat</h1>
@@ -117,13 +90,13 @@ class FwProjectsPage extends React.Component {
                         none of the data was encrypted so this project would be unsuited for real-world use.
                             <br />
                             <br />
-                            <p class="FwProjectsPage__projectItemLinks">
+                            <span className="FwProjectsPage__projectItemLinks">
                                 <a href="https://github.com/F1xw/p2p-chat" target="_blank" rel="noreferrer">GitHub Repo</a>
-                            </p>
+                            </span>
                         </p>
                     </div>
                 </div>
-                <div className={parseInt(this.state.activeIndex) === 4 ? "FwProjectsPage__projectItem no-active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={4}>
+                <div className={parseInt(this.state.activeIndex) === 3 ? "FwProjectsPage__projectItem no-active": "FwProjectsPage__projectItem"} onClick={this.handleClick} data-index={3}>
                     {/* <FaChevronRight /> */}
                     <div className="FwProjectsPage__projectItemText">
                         <h1>Small Projects</h1>
@@ -132,13 +105,13 @@ class FwProjectsPage extends React.Component {
                         Besides of all my larger projects there are several smaller projects published on GitHub. For example, a Discord Bot for recommending Gucci products and of course the code of this portfolio. 
                             <br />
                             <br />
-                            <p class="FwProjectsPage__projectItemLinks">
+                            <span className="FwProjectsPage__projectItemLinks">
                                 <a href="https://github.com/F1xw" target="_blank" rel="noreferrer">GitHub Profile</a>
                                 &nbsp;•&nbsp;
                                 <a href="https://github.com/F1xw/portfolio" target="_blank" rel="noreferrer">Portfolio Repo</a>
                                 &nbsp;•&nbsp;
                                 <a href="https://github.com/F1xw/guccibot" target="_blank" rel="noreferrer">Gucci Bot Repo</a>
-                            </p>
+                            </span>
                         </p>
                     </div>
                 </div>
