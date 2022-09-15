28df8202a34eb05b5110f05027a1bdecc39c3a99
Florian Weißmeier <flo@flowei.tech>
2022-09-15 09:55:40 +0200

    Updated "About Me" text to current situation

--- src/components/FwAboutMe/FwAboutMe.js
+++ src/components/FwAboutMe/FwAboutMe.js
@@ -16,8 +16,8 @@ class FwAboutMe extends React.Component {
                 <div className="FwAboutMe__text">
                     <h1>About Me</h1>
                     <p>
-                        I am 18 years old, living in Germany and currently working on my final school exams. Coding is a hobby for me, and I would like to turn this hobby into a career.
-                        My skills range from basic HTML to cross-platform Apps using Flutter. I can maintain my own server to host various applications I made,
+                        I am 18 years old, living in Germany and currently studying business and computer science in Düsseldorf. Coding is a hobby for me, and I would like to turn this hobby into a career.
+                        My skills range from basic HTML to cross-platform Apps using Flutter. I maintain my own server to host various applications I made,
                         and I enjoy tinkering with other open-source software. Most of my work is published on <a href="https://github.com/F1xw" target="_blank" rel='noreferrer'>GitHub</a>, free to use for everyone.
                         If you are interested in reading my paper on Peer-to-Peer Networking (written in German) click <a href="/Facharbeit-Florian_Weissmeier.pdf" target="_blank" rel='noreferrer'>here</a>.
                         For a depiction of my skills please click on “Next”.
