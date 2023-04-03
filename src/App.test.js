501ca5d7dd9b958e7791e09bdcb6d4b038d6ad60
Florian Weißmeier <flo@flowei.tech>
2023-04-03 17:31:45 +0200

    Removed unused files

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import FwLandingBubble from './components/FwLandingBubble/FwLandingBubble';
-
-test('renders landing page', () => {
-  const {container} = render(<FwLandingBubble />);
-  expect(container.getElementsByClassName("landingBubble__text")[0]).toBeInTheDocument();
-});
